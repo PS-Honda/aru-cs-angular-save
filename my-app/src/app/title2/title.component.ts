9a1d861dc97eac05666d4d4ec67aa3648cca603a
Pipit Srikumpa <15932021@aru.ac.th>
2021-05-22 15:28:48 +0700

    add example

--- my-app/src/app/title2/title.component.ts
+++ my-app/src/app/title2/title.component.ts
@@ -8,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class TitleComponent2 implements OnInit {
 
   constructor() { }
+  name: string = '';
+  surname: string = '';
+  studentId: string = '';
+
+
 
   ngOnInit(): void {
   }
